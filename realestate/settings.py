87183c8dbae9eeb0074c355fc5377077690468d6
Mayank Kumar <mayankumar214@gmail.com>
2026-06-26 20:54:47 +0530

    Force switch production settings to dj-database-url module

--- realestate/settings.py
+++ realestate/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
+# Production me Neon cloud DB uthayega, local fallback ke liye SQLite
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True if os.environ.get('DATABASE_URL') else False
+    )
 }
 
 
